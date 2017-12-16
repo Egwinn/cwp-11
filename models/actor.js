f894885b6f01ca855007daee16a13f91b0db6313
Marina <dubovik.m14@gmail.com>
2017-12-16 21:44:44 +0200

    add actor model

---
+++ models/actor.js
@@ -0,0 +1,28 @@
+
+class Actor {
+    constructor(actor) {
+        this.actor = {
+            "id": Date.now() + 1,
+            "name": actor.name,
+            "birth": actor.birth,
+            "films": Number(actor.films),
+            "liked": Number(actor.liked),
+            "photo": actor.photo,
+        }
+    }
+
+    get() {
+        return this.actor;
+    }
+
+    isValid() {
+        for(let elem in this.actor){
+            if(!this.actor[elem]) return false;
+        }
+        if( this.actor.films < 0 ||
+            this.actor.liked < 0 ) return false;
+        return true;
+    }
+}
+
+module.exports = Actor;
